599fffb5e373a7364228adcb5d524834f1054b20
miluge <gllmblndl@gmail.com>
2020-09-23 13:43:36 +0200

    Show all posts
    Show all posts for news page

---
+++ web/app/themes/wp_hoops/page-news.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * To generate specific templates for your pages you can use:
+ * /mytheme/templates/page-mypage.twig
+ * (which will still route through this PHP file)
+ * OR
+ * /mytheme/page-mypage.php
+ * (in which case you'll want to duplicate this file and save to the above path)
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+$query = array(
+    'post_type' => 'post',
+    'order_by' => 'ID',
+    'order' => 'DESC',
+);
+
+$context['posts'] = new Timber\PostQuery($query);
+$timber_post     = new Timber\Post();
+$context['sidebar'] = Timber::get_sidebar('sidebar.php');
+$context['post'] = $timber_post;
+Timber::render( array( 'pages/' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
