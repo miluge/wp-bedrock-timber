4860fc1fe44c807e4308f9d0501eea71601cf9ec
miluge <gllmblndl@gmail.com>
2020-09-25 09:10:38 +0200

    home tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,26 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    'web/app/themes/wp_hoops/**/*.twig',
+  ],
   theme: {
+    aspectRatio: {
+      none: 0,
+      square: [1, 1],
+      "16/9": [16, 9],
+      "4/3": [4, 3],
+      "21/9": [21, 9]
+    },
     extend: {},
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    aspectRatio: ['responsive'],
+  },
+  plugins: [
+    require("tailwindcss-responsive-embed"),
+    require("tailwindcss-aspect-ratio"),
+  ],
 }
